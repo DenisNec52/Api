d2ceca142188337deb1ecb0ac13874382f6ca99d
DenisNec52 <148898216+DenisNec52@users.noreply.github.com>
2024-02-05 18:26:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// // Initialize the Variables
+// let songIndex = 0;
+// let audioElement = new Audio('songs/1.mp3');
+// let masterPlay = document.getElementById('masterPlay');
+// let myProgressBar = document.getElementById('myProgressBar');
+// let gif = document.getElementById('gif');
+// let masterSongName = document.getElementById('masterSongName');
+// let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+// let urleminem = "https://striveschool-api.herokuapp.com/api/agenda";
+// // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+// .then((response) => response.json()) // Abbiamo una promise restituita qui
+// .then((json) => console.log(json)) // Qui abbiamo un oggetto JS (convertito dal JSON ricevuto dall'endpoint)
+// .catch((err) => console.log("Error detected: ", err) );
+
